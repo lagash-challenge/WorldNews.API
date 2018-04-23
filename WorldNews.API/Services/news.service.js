a64950c8404cf101abe1cb953ad63237dbf6cb7c
Jorge Luis Castro Felix <jorge.castro.felix@outlook.com>
2018-04-23 18:15:41 -0500

    Create post and retrive them

--- WorldNews.API/Services/news.service.js
+++ WorldNews.API/Services/news.service.js
@@ -1,13 +1,75 @@
 'use strict';
-var fs = require('fs');
 
+const uuidv4 = require('uuid/v4');
+var fs = require('fs');
+var moment = require('moment');
 function createNews(req, res) {
     console.log('POST - /api/createNews');
+    
+    if (!validateNewsPost(req.body, res)) {
+        return res.send("Invalid request");
+    }
+    var fileName = "postfeed.json";    
+    try {
+        var contents = fs.readFileSync(fileName).toString();       
+        if (contents != null || contents != "") {
+            var filedata = JSON.parse(contents);
+            var postRequest = req.body;
+            postRequest.id = uuidv4();
+            postRequest.date = moment().format(); 
+
+            filedata.push(req.body);
+
+            fs.writeFileSync(fileName, JSON.stringify(filedata), 'utf8', function () {
+                if (err) {
+                    return console.log(err);
+                }
+                return res.send(req.body);
+            });
+            return res.send(req.body);
+        }
+    }
+    catch (e) {
+        var newList = [];
+        var postRequest = req.body;        
+        postRequest.id = uuidv4();
+        postRequest.timeUTC = moment().format(); 
+        newList.push = postRequest;
+
+        fs.writeFileSync(fileName, JSON.stringify(newList), { flag: 'wx', encoding: 'utf8' }, function (err, data) {
+            return res.send(data);
+        })
+        return res.send(newList);;
+    }
+   
+    
+}
+
+function validateNewsPost(req, res) {
+
+    if (req.user == null || req.user == "") {
+        return res.send("Invalid user name");
+    }
+    if (req.text == null || req.text == "") {
+        return res.send("Invalid post text");
+    }
+    if (req.metaData == null) {
+        return res.send("Invalid post metadata");
+    }
+    if (req.metaData.keyphrases.length <= 0) {
+        return res.send("Invalid metadata keyphrases");
+    }
+    if (req.metaData.sentiment == null) {
+        return res.send("Invalid metadata sentiment");
+    }
+
+    return true;
 }
 
 function getNews(req, res) {
     console.log('GET - /api/getNews');
-    res.send(readFile("tweets.json"));    
+    var fileName = "postfeed.json";    
+    res.send(readFile(fileName));    
 }
 
 function updateNews(req, res) {
--- WorldNews.API/server.js
+++ WorldNews.API/server.js
@@ -4,11 +4,20 @@ const
     express = require('express'),
     bodyParser = require('body-parser'),
     app = express(),
+    cors = require('cors'),
     port = process.env.PORT || 3030;    
 
 this.init = function () {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors());
+
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Ocp-Apim-Subscription-Key");
+        res.header("Access-Control-Expose-Headers", "Traking");
+        next();
+    });
 
     require("./Services/worldnews.api.js")(app);
     app.listen(port);
